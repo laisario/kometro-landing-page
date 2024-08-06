c76a6dba8c300cc19c59b2d5cc84b4783aad5c4c
Laisa Muito Linda <laisa.rioverde@gmail.com>
2024-08-06 15:27:16 -0300

    nao consegui alterar rotas

--- layouts/components/YoutubePlayer.js
+++ layouts/components/YoutubePlayer.js
@@ -1,8 +1,10 @@
 import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-const YoutubePlayer = ({ id, title, ...others }) => {
-  return <LiteYouTubeEmbed id={id} title={title} {...others} />;
+const YoutubePlayer = ({ id, title, autoplay = false, ...others }) => {
+  const autoplayParam = autoplay ? 1 : 0;
+  const params = `autoplay=${autoplayParam}`;
+  return <LiteYouTubeEmbed id={id} title={title} params={params} {...others} />;
 };
 
 export default YoutubePlayer;
